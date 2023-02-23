d7833d7ce369ba194bb6f09b5bf2bd4dde586892
Ilia898 <107941505+Ilia898@users.noreply.github.com>
2023-02-23 21:23:56 +0100

    23.02.23-4

--- pagesandparts/src/app/pages/mein-konto/kontodetails/kontodetails/kontodetails.component.ts
+++ pagesandparts/src/app/pages/mein-konto/kontodetails/kontodetails/kontodetails.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-kontodetails',
   templateUrl: './kontodetails.component.html',
-  styleUrls: ['./kontodetails.component.css']
+  styleUrls: ['./kontodetails.component.scss']
 })
 export class KontodetailsComponent implements OnInit {
 
