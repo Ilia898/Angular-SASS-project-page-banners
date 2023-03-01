6963cf3d5f7059d40248102fc4a091ae6c5fa6f5
Ilia898 <107941505+Ilia898@users.noreply.github.com>
2023-03-01 18:54:07 +0100

    01.03.23-2

--- pagesandparts/src/app/pages/product/product.component.ts
+++ pagesandparts/src/app/pages/product/product.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import {faStar, faStarHalfStroke} from '@fortawesome/free-solid-svg-icons';
 import { faStar as farStar} from '@fortawesome/free-regular-svg-icons';
-import description  from '../../Json/productdescription.json';
+import products  from '../../Json/productdescription.json';
 
 @Component({
   selector: 'app-product',
   templateUrl: './product.component.html',
   styleUrls: ['./product.component.scss']
 })
+
 export class ProductComponent {
 
   fullStar = faStar;
@@ -17,27 +18,19 @@ export class ProductComponent {
   valueRate = 162;
   totalStars = 5;
 
-  priceValue = 25.25;
+  product = products.mainProducts[0];
 
-  description: any = description;
 
-  productsImage =[
-    {"id": "0", "url": "../assets/product-slider-image/HOME.jpg"},
-    {"id": "1", "url": "../assets/product-slider-image/HOME4.jpg"},
-    {"id": "2", "url": "../assets/product-slider-image/HOME7.jpg"},
-    {"id": "3", "url": "../assets/product-slider-image/HOME17.jpg"},
-    {"id": "4", "url": "../assets/product-slider-image/HOME48.jpg"}
-  ]
-
-  productTitle: string = 'Erwecken Sie die Fantasie Ihres Kindes mit einem personalisierten Kinder-Magazin, das eine umfangreiche Tier-Enzyklopädie, kurzweilige Geschichten und kreative Gestaltungsmöglichkeiten bietet';
-
-  mainImage = this.productsImage[0].url;
+  mainImage = this.product.productsImage[0].url;
   activImage = this.mainImage;
 
 
   imagePreview(proImage: any){
-    this.mainImage = this.productsImage[proImage.id].url;
+    this.mainImage = this.product.productsImage[proImage.id].url;
+  }
 
+  test(){
+    console.log(this.product)
   }
 
 }
